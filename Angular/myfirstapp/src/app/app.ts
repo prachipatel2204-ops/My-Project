fe627437b0ad6d591c9e0f5f89782b32b1a185db
prachipatel2204-ops <prachipatel2204@gmail.com>
2025-12-08 18:17:19 +0530

    Changes in app.ts

---
+++ Angular/myfirstapp/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+  template:
+  `<h1>HEllo</h1>`,
+  styles:`h1{color:red}`
+})
+export class App {
+  protected readonly title = signal('myfirstapp');
+}
